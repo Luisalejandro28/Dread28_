c8d793764579a4c1519d2c0385478e39de2ae855
Luis Alejandro Machado Orozco <luis.machadox@gmail.com>
2022-11-28 16:46:34 -0300

    v 0.1.0

--- JavaScript/bosstiary.js
+++ JavaScript/bosstiary.js
@@ -1,22 +1,16 @@
-const bosstiary = [
-    { tipo: "Bane" , nombre: "Jailer", cantidad: "300 Kill's", localizacion: "Falcon Bastion"},
-    { tipo: "Archfoe", nombre: "Anomaly", cantidad: "60 Kill's", localizacion: "The Void (Ankrahmun, Svargrond, Zao)"},
-    { tipo: "Nenesis", nombre: "Alptramun", cantidad: "5 Kill's", localizacion: "Dream Scar"},
-    { tipo: "Bane", nombre: "Black Knight", cantidad: "300 Kill's", localizacion: "Black Knight Villa"},
-  ];
-  
-    let nombre = prompt("Ingrese el Nombre del Boss");
-    let encontrado = bosstiary.find(boss => boss.nombre === nombre);
-    
-     if(encontrado){
-       let mensaje = `
-        Nombre: ${encontrado.nombre}
-        Tipo: ${encontrado.tipo}
-        Cantidad: ${encontrado.cantidad}
-        Localizacion: ${encontrado.localizacion}
-       `;
-    
-       alert(mensaje);
-     }else{
-       alert("Boss no encontrado");
-     }
+const contenedorBosses = document.getElementById("contenedor-bosses")
+
+bosses.forEach((boss) => {
+    const div = document.createElement("div")
+    div.classList.add("producto")
+    div.innerHTML = ` 
+        <img src= ${boss.img} alt = "img">
+        <h5>${boss.nombre}</h5>
+        <p><b>Tipo:</b> ${boss.tipo}</p>
+        <p><b>Cantidad:</b> ${boss.cantidad}</p>
+        <p><b>Localizacion:</b> ${boss.localizacion}</p>
+    `
+    contenedorBosses.appendChild(div)
+})
+
+
